093dfaffdfe2636615774ed6548641526fadf549
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-12 23:27:26 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import { UserProvider } from "../context/User";
 
 function App() {
   const [theme, setTheme] = useState("dark");
